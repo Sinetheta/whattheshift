5b397d39d8c52cb799db5da75c54db48d8e12a92
Kevin Attfield <k.attfield@gmail.com>
2018-05-29 12:22:08 -0700

    Add update image api

--- app/controllers/api/images_controller.rb
+++ app/controllers/api/images_controller.rb
@@ -1,12 +1,22 @@
 module Api
   class ImagesController < ApiController
-    before_action :set_image, only: [:show]
+    before_action :set_image, only: [:show, :update]
 
     def show
       json_response(@image)
     end
 
+    def update
+      @image.update(image_params)
+      json_response(@image)
+    end
+
     private
+
+    def image_params
+      params.require(:image)
+            .permit [:description, :identifier, :signature]
+    end
 
     def set_image
       @image = Image.find(params[:id])
