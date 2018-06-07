c7c965690196b13543857de8b327d3bd72428d86
Kevin Attfield <k.attfield@gmail.com>
2018-06-06 20:55:31 -0400

    Find or create revision images before save

--- app/models/revision.rb
+++ app/models/revision.rb
@@ -2,4 +2,15 @@ class Revision < ApplicationRecord
   has_and_belongs_to_many :images
 
   accepts_nested_attributes_for :images
+
+  before_save :find_or_create_images
+
+  private
+
+  # Avoid creating new images when existing images can be shared between revisions
+  def find_or_create_images
+    self.images = images.map do |image|
+      Image.find_by(image.attributes.compact) || image
+    end
+  end
 end
