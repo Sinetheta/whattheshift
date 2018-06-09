15a6de3e58dddbcbb5f31e6b4fa292de2bbb1454
Kevin Attfield <k.attfield@gmail.com>
2018-06-09 15:08:19 -0400

    Add create comparison interface for revisions

--- app/controllers/comparisons_controller.rb
+++ app/controllers/comparisons_controller.rb
@@ -1,9 +1,28 @@
 class ComparisonsController < ApplicationController
+  def create
+    require 'whattheshift/revision_compare'
+    @comparison = WhatTheShift::RevisionCompare.new.call(*revisions)
+    render :show
+  end
+
   def index
     @comparisons = Comparison.includes(:image_diffs).all
   end
 
   def show
     @comparison = Comparison.includes(:image_diffs).find(params[:id])
+  end
+
+  private
+
+  def comparision_params
+    params.require(:comparison)
+          .permit(revision_ids: [])
+  end
+
+  def revisions
+    revision_ids = comparision_params[:revision_ids]
+    raise 'Must select 2 Revisions' unless revision_ids.length == 2
+    Revision.where(id: comparision_params[:revision_ids])
   end
 end
