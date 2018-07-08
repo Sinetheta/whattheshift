3869b348d91a4b9e89308e8213b1928a503bfaab
Kevin Attfield <kevin@stembolt.com>
2018-07-07 20:20:59 -0700

    Allow github login for existing users

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,13 +1,9 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
     user = User.from_omniauth(request.env['omniauth.auth'])
-    if user.persisted?
-      sign_in_and_redirect user, event: :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, kind: 'Github') if is_navigational_format?
-    else
-      session['devise.github_data'] = request.env['omniauth.auth']
-      redirect_to new_user_registration_url
-    end
+    user.link_account_from_omniauth(request.env['omniauth.auth'])
+    sign_in_and_redirect user, event: :authentication #this will throw if user is not activated
+    set_flash_message(:notice, :success, kind: 'Github') if is_navigational_format?
   end
 
   def failure
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,23 @@ class User < ApplicationRecord
          :trackable,
          :validatable
 
-  # TODO: handle users who already have an account by email
   def self.from_omniauth(auth)
-    where(provider: auth[:provider], uid: auth[:uid]).first_or_create do |user|
-      user.email = auth[:info][:email]
+    where(email: auth[:info][:email]).or(
+      where(provider: auth[:provider], uid: auth[:uid])
+    ).first_or_create do |user|
       user.password = Devise.friendly_token[0,20]
-      user.oauth_token = auth[:credentials][:token]
-      user.oauth_expires_at = Time.at(auth[:credentials][:expires_at]) if auth[:credentials].try(:[], :expires)
-      # If you are using confirmable and the provider(s) you use validate emails,
+      # If you are using confirmable and the provider(s) you use validate emails, 
       # uncomment the line below to skip the confirmation emails.
       # user.skip_confirmation!
     end
+  end
+
+  def link_account_from_omniauth(auth)
+    self.email = auth[:info][:email] if self.email.blank?
+    self.provider = auth[:provider]
+    self.oauth_token = auth[:credentials][:token]
+    self.oauth_expires_at = Time.at(auth[:credentials][:expires]) if auth[:credentials].try(:[], :expires)
+    self.uid = auth[:uid]
+    self.save
   end
 end
