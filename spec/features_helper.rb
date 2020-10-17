6484745f49e10226a07cc796bc181bf24acc5589
Kevin Attfield <k.attfield@gmail.com>
2020-10-17 15:43:54 -0700

    Read visdiff api key from ENV
    Our feature specs are now going to need a project api_key.

--- spec/features_helper.rb
+++ spec/features_helper.rb
@@ -7,6 +7,6 @@ require 'visdiff/rspec'
 RSpec.configure do |config|
   config.visdiff.enable
   config.visdiff.base_url = Figaro.env.visdiff_base_url
-  config.visdiff.api_key = 'not-implemented'
+  config.visdiff.api_key = Figaro.env.visdiff_api_key
   config.visdiff.revision_from_git!
 end
