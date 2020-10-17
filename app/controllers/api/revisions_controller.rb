d35a27cdf6756d755fe0cfa00a8a7fbdab8f43ed
Kevin Attfield <k.attfield@gmail.com>
2020-10-17 15:44:53 -0700

    Use Api key to load correct project
    It's no longer possible to generate free-floating revisions.
    Currently they must all be associated to a specific scenario.
    For now we're just going to grab the first scenario of project
    that the api key belongs to.
    
    This will allow us to get our feature specs working again and
    make the API compatible with all of the changes that were made to
    support scripted scenarios.

--- app/controllers/api/revisions_controller.rb
+++ app/controllers/api/revisions_controller.rb
@@ -1,17 +1,24 @@
 module Api
   class RevisionsController < ApiController
     before_action :set_revision, only: [:show]
+    before_action :load_scenario, only: [:create]
 
     def show
       render(@revision)
     end
 
     def create
-      @revision = Revision.create!(revision_params)
+      @revision = @scenario.revisions.create!(revision_params)
       render(@revision, status: :created)
     end
 
     private
+
+    def load_scenario
+      api_key = params.require(:api_key)
+      project = ProjectToken.find(api_key).project
+      @scenario = project.scenarios.first
+    end
 
     def revision_params
       params.require(:revision)
--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -1,5 +1,6 @@
 class Scenario < ApplicationRecord
   belongs_to :project
+  has_many :revisions
   has_one :script, dependent: :destroy
 
   accepts_nested_attributes_for :script
