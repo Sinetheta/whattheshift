b9ef8329b4c28e0817b4a201e8b748f4957fdf85
Kevin Attfield <k.attfield@gmail.com>
2020-10-20 15:32:39 -0700

    Add auth verification to application controller
    This isn't foolproof but it's one way to avoid mistakenly
    forgetting to authorize an action. If it gets annoying I can
    pull it out later.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
   include Pundit
+  after_action :verify_authorized, except: :index
+  after_action :verify_policy_scoped, only: :index
+
   protect_from_forgery with: :reset_session
 end
