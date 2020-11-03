b738e977d9ad67a0239e38529b1a541554556a7d
Kevin Attfield <k.attfield@gmail.com>
2020-11-03 15:59:03 -0800

    Allow public read of revisions

--- app/controllers/revisions_controller.rb
+++ app/controllers/revisions_controller.rb
@@ -1,4 +1,7 @@
 class RevisionsController < ApplicationController
+  skip_after_action :verify_authorized
+  skip_after_action :verify_policy_scoped
+
   def index
     @revisions = Revision.includes(:images).all
   end
