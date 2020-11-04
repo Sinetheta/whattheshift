5a0fa98154f05f0d7df58c63502fa7d897007b08
Kevin Attfield <k.attfield@gmail.com>
2020-11-03 19:48:42 -0800

    jigger GOOGLE_CHROME_SHIM

--- config/initializers/capybara.rb
+++ config/initializers/capybara.rb
@@ -5,7 +5,9 @@ Capybara.register_driver :chrome_headless do |app|
   # heroku-buildpack-google-chrome puts Chrome binary in a non-standard location
   # https://github.com/heroku/heroku-buildpack-google-chrome#selenium
   chrome_binary_path = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
-  browser_options.binary = chrome_binary_path if chrome_binary_path
+  if chrome_binary_path
+    ::Selenium::WebDriver::Chrome.path = chrome_binary_path
+  end
 
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
 end
